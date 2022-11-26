973e5962c21b16a19a1e6e16a21bd9f7d65b4a01
kmarellano <kurt.arellano@stratpoint.com>
2022-11-26 11:38:43 +0800

    backup branch for heroku

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const db = require("./startup/db");
 require("./routers")(app);
 
 app.use((err, req, res, next) => {
-	let { statusCode, message } = err;
-	statusCode = statusCode || 500;
-	return res.status(statusCode).json({ message });
+    let { statusCode, message } = err;
+    statusCode = statusCode || 500;
+    return res.status(statusCode).json({ message });
 });
 
 app.use((req, res, next) => {
-	res.send("No page found");
+    res.send("Page not found");
 });
 
 db()
-	.then(() => {
-		app.listen(process.env.PORT, () => {
-			console.log(`server started at port: ${process.env.PORT}`);
-		});
-	})
-	.catch((err) => {
-		throw err;
-	});
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server started at port: ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        throw err;
+    });
