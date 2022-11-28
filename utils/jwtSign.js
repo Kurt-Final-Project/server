d616c9fab85135ec3b237749aacf7040c9af3007
kmarellano <kurt.arellano@stratpoint.com>
2022-11-28 08:41:07 +0800

    jwt expiration

--- utils/jwtSign.js
+++ utils/jwtSign.js
@@ -1,11 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (payload) => {
-	return jwt.sign(
-		payload,
-		process.env.JWT_SECRET
-		// 	, {
-		// 	expiresIn: "1h",
-		// }
-	);
+    return jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+    });
 };
